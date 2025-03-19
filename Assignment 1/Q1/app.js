691b6e5750a71a31cde288cef8a20a65d4c0d037
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-03-19 12:53:17 +0530

    Add files via upload
    Assignment 1 and 2

---
+++ Assignment 1/Q1/app.js
@@ -0,0 +1,9 @@
+const math = require('./mathOperations');
+
+const num1 = 20;
+const num2 = 10;
+
+console.log(`Addition: ${math.add(num1, num2)}`);         
+console.log(`Subtraction: ${math.subtract(num1, num2)}`); 
+console.log(`Multiplication: ${math.multiply(num1, num2)}`); 
+console.log(`Division: ${math.divide(num1, num2)}`);     
